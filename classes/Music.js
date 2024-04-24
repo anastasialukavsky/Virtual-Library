e07240c2d1a79581d87fa808925b9e414f282f84
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-23 21:15:23 -0400

    add music class

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,21 @@
 // import the Media class:
-
+const Media = require('./Media');
 // create your Music class:
+class Music extends Media {
+  constructor(title, year, genre, artist, length) {
+    super(title, year, genre);
+    this.artist = artist;
+    this.length = length;
+  }
 
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+  }
+
+  static shortestAlbum(albums) {
+    if(!Array.isArray(albums)) return null;
+    return albums.reduce((currentSong, prevSong) => currentSong.length > prevSong.length ? prevSong : currentSong)
+  }
+}
 // don't change below
 module.exports = Music;
