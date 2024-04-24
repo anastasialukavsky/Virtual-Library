99a3679d9b6b95feba97882fb0ab32ec43d1a1f8
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-23 21:14:53 -0400

    add book class

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,27 @@
 // import the Media class:
-
+const Media = require('./Media')
 // create your Book class:
+class Book extends Media {
+  constructor(title, year, genre,author, numPages, rating) {
+    super(title, year, genre);
+    this.author = author;
+    this.numPages = numPages;
+    this.rating = rating;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static highestRating(books) {
+    if (!Array.isArray(books) || books.length === 0) {
+      return null;
+    }
+    return books.reduce((prevBook, currentBook) => {
+      return prevBook.rating > currentBook.rating ? prevBook : currentBook;
+    });
+  }
+}
 
 // don't change below
 module.exports = Book;
